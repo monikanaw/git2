bccae69f75deec85e5cadbb306a703c2affadcb7
Monika Nawrocka <mnawrocka@oakfusion.pl>
2020-04-01 18:23:10 +0200

    moni222a

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edihhhht <code>src/App.js</code> and save to reload.
+          Edihhhht <code>src/App.lljs</code> and save to reload.
         </p>
         <a
           className="App-link"
+          hhh
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
